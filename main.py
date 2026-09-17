6e002dbcb3637e772cbef678e00b1978703a62cb
DannyM2027 <daniel.matut2027@gbwl.org>
2026-09-17 16:34:16 +0000

    day 1 notes

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+ # for ints in python, just: x = 5 
+ # no camel case, but use underscores: more_than
+ # CANNOT start w number, special char, no keywords like and if true false for variables
+ #careful with int list , str
+
+ # int x= 5, floats: x = 500.1, complex: x = 30j
+
+grade = 92.87
+print("your grade is:")
+print(int(grade))
+
+# int/int = float, int + float = float
+
