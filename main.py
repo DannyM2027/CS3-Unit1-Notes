67de97aad144f34e373387a5c87d76aacbce5898
DannyM2027 <daniel.matut2027@gbwl.org>
2026-09-18 12:54:26 +0000

    day 2 notes

--- main.py
+++ main.py
@@ -17,4 +17,21 @@ print("your grade is:")
 print(int(grade))
 
 # int/int = float, int + float = float
+x = 5
+x = float(x)
+print(type(x)) # declared
+print(x)
 
+# quotes for strings (single apostrophes)
+# triple quotes
+print("""helo""")
+class_size = 5
+
+print('hello class of ' + str(class_size) +'!')
+
+
+# printing w fStrings
+print(f"helo class of {class_size}!")
+
+matthew_age = 1
+print(f"matthew is {matthew_age} years old!")
